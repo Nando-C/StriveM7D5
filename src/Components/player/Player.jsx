896f66ee58e21f461fdb832c8057c593fb468097
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-08-17 21:38:32 +0100

    adds Footer structure

---
+++ src/Components/player/Player.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+import './player.css'
+
+class Player extends Component {
+    state = {  }
+    render() { 
+        return (
+            <>
+            <h3>Player</h3>
+            </>
+        );
+    }
+}
+ 
+export default Player;
---
+++ src/Components/volume/Volume.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+import './volume.css'
+
+class Volume extends Component {
+    state = {  }
+    render() { 
+        return (
+            <>
+            <h3>Volume</h3>
+            </>
+        );
+    }
+}
+ 
+export default Volume;
