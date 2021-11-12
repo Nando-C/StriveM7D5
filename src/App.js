8ba7923b0be0e4b17cb6b937b69552694290c2eb
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-11-12 16:14:38 +0000

    layout mods

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 // import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Row } from 'react-bootstrap'
-import SideMenu from './Components/sideMenu/SideMenu'
-import Footer from './Components/footer/Footer'
-import Home from './Components/Home'
-import Artist from './Components/Artist'
-import AlbumPage from './Components/AlbumPage'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
+import SideMenu from "./Components/sideMenu/SideMenu";
+import Footer from "./Components/footer/Footer";
+import Home from "./Components/Home";
+import Artist from "./Components/Artist";
+import AlbumPage from "./Components/AlbumPage";
 
 function App() {
   return (
     // <div className="App">
-      <Router>
-        <Row>
-          <SideMenu />
-          <Route exact path='/' component={Home} />
-          <Route path='/Artist/:artistId' component={Artist} />
-          <Route path='/Album/:albumId' component={AlbumPage} />
-        </Row>
+    <Router>
+      <Container fluid className="d-flex p-0">
+        <SideMenu />
+        <Route exact path="/" component={Home} />
+        <Route path="/Artist/:artistId" component={Artist} />
+        <Route path="/Album/:albumId" component={AlbumPage} />
         <Footer />
-      </Router>
-     
+      </Container>
+    </Router>
+
     // </div>
   );
 }
