5e717a717c9e39c6a3e88da74c6ef3cc2646001f
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-11-12 22:25:36 +0000

    adds create playlist and liked songs with styles

--- src/Components/SideNavBar/SideNavBar.jsx
+++ src/Components/SideNavBar/SideNavBar.jsx
@@ -122,7 +122,51 @@ const SideNavBar = () => {
               </small>
             </span>
           </Nav.Link>
+          <Nav.Link className="mt-4 d-flex">
+            <div id="plus-icon" className="d-flex">
+              <svg
+                role="img"
+                height="12"
+                width="12"
+                viewBox="0 0 16 16"
+                // class="Svg-sc-1bi12j5-0 fIDrcz"
+              >
+                <path fill="none" d="M0 0h16v16H0z"></path>
+                <path fill="black" d="M14 7H9V2H7v5H2v2h5v5h2V9h5z"></path>
+              </svg>
+            </div>
+            <span>
+              <small>
+                <strong>Create Playlist</strong>
+              </small>
+            </span>
+          </Nav.Link>
+          <Nav.Link className="d-flex">
+            <div id="heart-songs-icon" className="d-flex">
+              <svg
+                role="img"
+                height="12"
+                width="12"
+                viewBox="0 0 16 16"
+                class="Svg-sc-1bi12j5-0 fIDrcz"
+              >
+                <path fill="none" d="M0 0h16v16H0z"></path>
+                <path
+                  fill="white"
+                  d="M13.797 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253c-.77.77-1.194 1.794-1.194 2.883s.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195a4.052 4.052 0 001.195-2.883 4.057 4.057 0 00-1.196-2.883z"
+                ></path>
+              </svg>
+            </div>
+            <span>
+              <small>
+                <strong>Liked Songs</strong>
+              </small>
+            </span>
+          </Nav.Link>
         </Nav>
+        <Col>
+          <hr />
+        </Col>
       </Row>
     </Container>
   );
