03709e8fcfb7ebef316abe902f6a47ac8326298e
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-11-13 16:38:46 +0000

    adds player footer & NowPlaying converts to functional component

--- src/Components/nowPlaying/NowPlaying.jsx
+++ src/Components/nowPlaying/NowPlaying.jsx
@@ -1,30 +1,53 @@
-import { Component } from 'react'
-import { Row, Col, Image } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-import './nowPlaying.css';
+import { Row, Col, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./nowPlaying.css";
 
-class NowPlaying extends Component {
-    state = {  }
-    render() { 
-        return (  
-            // <div>
-            <Row className='justify-content-start no-gutters'>
-                <div className='cover-img'>
-                    <Image src='https://picsum.photos/200' fluid/>
-                </div>
-                <div>
-                    <div className='info-playing'>
-                        <div><span>Everlong</span></div>
-                        <Link to=''><span><small>Foo Fighters</small></span></Link>
-                    </div>
-                </div>
-                <div className='like-heart'>
-                    <svg role="img" height="16" width="16" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 fIDrcz"><path fill="none" d="M0 0h16v16H0z"></path><path fill="#B1B1B1" d="M13.797 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253c-.77.77-1.194 1.794-1.194 2.883s.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195a4.052 4.052 0 001.195-2.883 4.057 4.057 0 00-1.196-2.883z"></path></svg>
-                </div>
-            </Row>
-            // </div>
-        )
-    }
-}
- 
+const NowPlaying = () => {
+  return (
+    <Row className="NowPlaying justify-content-start align-items-center">
+      <div className="cover-img">
+        <Image src="https://picsum.photos/200" fluid />
+      </div>
+      <div>
+        <div className="info-playing">
+          <div>
+            <span>Everlong</span>
+          </div>
+          <Link to="">
+            <span>
+              <small>Foo Fighters</small>
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div className="like-heart">
+        <svg
+          role="img"
+          height="16"
+          width="16"
+          viewBox="0 0 16 16"
+          class="Svg-sc-1bi12j5-0 fIDrcz"
+        >
+          <path fill="none" d="M0 0h16v16H0z"></path>
+          <path
+            fill="#B1B1B1"
+            d="M13.797 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253c-.77.77-1.194 1.794-1.194 2.883s.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195a4.052 4.052 0 001.195-2.883 4.057 4.057 0 00-1.196-2.883z"
+          ></path>
+        </svg>
+      </div>
+      <div className="pip-toggle">
+        <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+          <g fill="currentColor" fill-rule="evenodd">
+            <path
+              d="M1 3v9h14V3H1zm0-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"
+              fill-rule="nonzero"
+            ></path>
+            <path d="M10 8h4v3h-4z"></path>
+          </g>
+        </svg>
+      </div>
+    </Row>
+  );
+};
+
 export default NowPlaying;
--- src/Components/player/Player.jsx
+++ src/Components/player/Player.jsx
@@ -1,12 +1,27 @@
-import { Container, Row } from "react-bootstrap"
-import "./Player.css"
+import { Col, Container, Row } from "react-bootstrap";
+import NowPlaying from "../nowPlaying/NowPlaying";
+import "./Player.css";
 
 const Player = () => {
-    return(
-        <Container className="Player" fluid>
-            <Row></Row>
-        </Container>
-    )
-}
+  return (
+    <Container className="Player" fluid>
+      <Row className="px-3 h-100 align-items-center">
+        <Col>
+          <NowPlaying />
+        </Col>
+        <Col>
+          <Row className="justify-content-center align-items-center h-100">
+            Player controls
+          </Row>
+        </Col>
+        <Col>
+          <Row className="justify-content-center align-items-center h-100">
+            Volume
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Player
+export default Player;
