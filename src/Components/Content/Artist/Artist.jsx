6a54bb349ee8d0dce287b9937499ab363e84ee1a
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-11-17 20:23:02 +0000

    adds option bar to Artist page

--- src/Components/Content/Artist/Artist.jsx
+++ src/Components/Content/Artist/Artist.jsx
@@ -39,14 +39,68 @@ class Artist extends Component {
           <>
             <Row className="banner m-0">
               <Image src={this.state.artist.picture_xl} />
-              <Row className="info flex-column justify-content-end">
+              <Row className="banner-text flex-column justify-content-end">
                 <h1>{this.state.artist.name}</h1>
                 <h4>
                   {this.state.artist.nb_fan.toLocaleString()} Monthly Listeners
                 </h4>
               </Row>
             </Row>
-            <Row className="mx-3">Albums</Row>
+            <Row className="artist-content m-0 px-3">
+              <Row className="options mx-0">
+                <div className="play-btn">
+                  <svg
+                    height="32"
+                    role="img"
+                    width="32"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <polygon
+                      points="21.57 12 5.98 3 5.98 21 21.57 12"
+                      fill="currentColor"
+                    ></polygon>
+                  </svg>
+                  {/* <svg
+                    height="32"
+                    role="img"
+                    width="32"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <rect
+                      x="5"
+                      y="3"
+                      width="4"
+                      height="18"
+                      fill="currentColor"
+                    ></rect>
+                    <rect
+                      x="15"
+                      y="3"
+                      width="4"
+                      height="18"
+                      fill="currentColor"
+                    ></rect>
+                  </svg> */}
+                </div>
+                <div>
+                  <div className="follow-btn">FOLLOW</div>
+                </div>
+                <div className="ellipsis-btn">
+                  <svg
+                    role="img"
+                    height="32"
+                    width="32"
+                    viewBox="0 0 32 32"
+                    class="Svg-sc-1bi12j5-0 gSLhUO"
+                  >
+                    <path d="M5.998 13.999A2 2 0 105.999 18a2 2 0 00-.001-4zm10.001 0A2 2 0 1016 18a2 2 0 000-4zm10.001 0A2 2 0 1026.001 18 2 2 0 0026 14z"></path>
+                  </svg>
+                </div>
+              </Row>
+              <Row className="mx-0 ">Albums</Row>
+            </Row>
           </>
         )}
       </Container>
