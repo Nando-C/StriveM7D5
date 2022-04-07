e014e406b39ad47c86648c0aed739bcb15f8387d
Nando-C <73954373+Nando-C@users.noreply.github.com>
2022-04-07 18:33:07 +0100

    connects album trackList to audio player

--- src/Components/TrackList/TrackList.jsx
+++ src/Components/TrackList/TrackList.jsx
@@ -1,6 +1,7 @@
 import { Col, Container, Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { selectSong, setIsPlaying } from "../../redux/slices/currentSong";
 import "./TrackList.css";
 
 const TrackList = () => {
@@ -8,6 +9,18 @@ const TrackList = () => {
   const trackList = useSelector(
     (state) => state.selectedAlbum.album.tracks.data
   );
+
+  const isPlaying = useSelector((state) => state.currentSong.isPlaying);
+  const selectedTrack = useSelector((state) => state.currentSong.track);
+
+  const dispatch = useDispatch();
+
+  const togglePlayPause = (track) => {
+    if (selectedTrack.id !== track.id) {
+      dispatch(selectSong(track));
+    }
+    dispatch(setIsPlaying(!isPlaying));
+  };
 
   return (
     <Container className="TrackList" fluid>
@@ -28,13 +41,7 @@ const TrackList = () => {
             </Col>
             <Col xs={3}>
               <div className="time-icon">
-                <svg
-                  role="img"
-                  height="16"
-                  width="16"
-                  viewBox="0 0 16 16"
-                  // class="Svg-sc-1bi12j5-0 gSLhUO"
-                >
+                <svg role="img" height="16" width="16" viewBox="0 0 16 16">
                   <path d="M7.999 3h-1v5h3V7h-2V3zM7.5 0a7.5 7.5 0 100 15 7.5 7.5 0 000-15zm0 14C3.916 14 1 11.084 1 7.5S3.916 1 7.5 1 14 3.916 14 7.5 11.084 14 7.5 14z"></path>
                   <path fill="none" d="M16 0v16H0V0z"></path>
                 </svg>
@@ -44,19 +51,33 @@ const TrackList = () => {
           {trackList?.map((track, i) => (
             <Row key={track.id} className="table-row">
               <Col className="d-flex flex-row justify-content-center" xs={1}>
-                <div className="track-number">{i + 1}</div>
-                <div className="play-btn">
-                  <svg
-                    height="16"
-                    role="img"
-                    width="16"
-                    viewBox="0 0 24 24"
-                    // class="WaNY0e0sSL2GquK9BIKv"
-                  >
-                    <polygon
+                <div
+                  className="track-number"
+                  style={{
+                    display:
+                      isPlaying && selectedTrack.id === track.id && "none",
+                  }}
+                >
+                  {i + 1}
+                </div>
+                <div
+                  className="play-btn"
+                  style={{
+                    display:
+                      isPlaying && selectedTrack.id === track.id && "flex",
+                  }}
+                  onClick={() => togglePlayPause(track)}
+                >
+                  <svg height="16" role="img" width="16" viewBox="0 0 16 16">
+                    {isPlaying && selectedTrack.id === track.id ? (
+                      <path fill="white" d="M3 2h3v12H3zm7 0h3v12h-3z"></path>
+                    ) : (
+                      <path fill="white" d="M4.018 14L14.41 8 4.018 2z"></path>
+                    )}
+                    {/* <polygon
                       points="21.57 12 5.98 3 5.98 21 21.57 12"
                       fill="white"
-                    ></polygon>
+                    ></polygon> */}
                   </svg>
                 </div>
               </Col>
@@ -75,13 +96,7 @@ const TrackList = () => {
               <Col xs="auto" sm={2} className="p-0">
                 <Row className="align-items-center p-0 m-0">
                   <Col className="heart-icon d-none d-md-flex">
-                    <svg
-                      role="img"
-                      height="16"
-                      width="16"
-                      viewBox="0 0 16 16"
-                      // class="Svg-sc-1bi12j5-0 gSLhUO"
-                    >
+                    <svg role="img" height="16" width="16" viewBox="0 0 16 16">
                       <path d="M13.764 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253A4.05 4.05 0 00.974 5.61c0 1.089.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195A4.052 4.052 0 0014.96 5.61a4.057 4.057 0 00-1.196-2.883zm-.722 5.098L8.58 13.048c-.307.36-.921.36-1.228 0L2.864 7.797a3.072 3.072 0 01-.905-2.187c0-.826.321-1.603.905-2.187a3.091 3.091 0 012.191-.913 3.05 3.05 0 011.957.709c.041.036.408.351.954.351.531 0 .906-.31.94-.34a3.075 3.075 0 014.161.192 3.1 3.1 0 01-.025 4.403z"></path>
                     </svg>
                   </Col>
