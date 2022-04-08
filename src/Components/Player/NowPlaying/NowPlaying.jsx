5fa10ad740fab9608fb27556756d504bed7c0e66
Nando-C <73954373+Nando-C@users.noreply.github.com>
2022-04-08 17:50:35 +0100

    Improves style to Footer /Player
    Now Playing component refactored to have song title and artist name truncated

--- src/Components/Player/NowPlaying/NowPlaying.jsx
+++ src/Components/Player/NowPlaying/NowPlaying.jsx
@@ -11,46 +11,40 @@ const NowPlaying = () => {
       xs="auto"
       className="NowPlaying justify-content-start align-items-center align-content-center"
     >
-      <Col className="cover-img">
+      <div className="cover-img">
         <Link to={`/Album/${currentSong.album.id}`}>
           <Image src={currentSong.album.cover_small} fluid />
         </Link>
-      </Col>
-      <Col className="p-0">
+      </div>
+      <div className="info d-flex p-0">
         <div className="info-playing">
           <div>
-            <span>{currentSong.title_short}</span>
+            <div>{currentSong.title_short}</div>
           </div>
-          <Link to={`/Artist/${currentSong.artist.id}`}>
-            <span>
-              <small>{currentSong.artist.name}</small>
-            </span>
-          </Link>
+          <div>
+            <Link to={`/Artist/${currentSong.artist.id}`}>
+              {currentSong.artist.name}
+            </Link>
+          </div>
         </div>
-      </Col>
-      <Col className="like-heart d-none d-sm-flex">
-        <svg
-          role="img"
-          height="16"
-          width="16"
-          viewBox="0 0 16 16"
-          // class="Svg-sc-1bi12j5-0 fIDrcz"
-        >
-          <path d="M13.764 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253A4.05 4.05 0 00.974 5.61c0 1.089.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195A4.052 4.052 0 0014.96 5.61a4.057 4.057 0 00-1.196-2.883zm-.722 5.098L8.58 13.048c-.307.36-.921.36-1.228 0L2.864 7.797a3.072 3.072 0 01-.905-2.187c0-.826.321-1.603.905-2.187a3.091 3.091 0 012.191-.913 3.05 3.05 0 011.957.709c.041.036.408.351.954.351.531 0 .906-.31.94-.34a3.075 3.075 0 014.161.192 3.1 3.1 0 01-.025 4.403z"></path>
-          {/* <path fill="none" d="M0 0h16v16H0z"></path> */}
-          {/* <path
+        <div className="like-heart d-none d-sm-block">
+          <svg role="img" height="16" width="16" viewBox="0 0 16 16">
+            <path d="M13.764 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253A4.05 4.05 0 00.974 5.61c0 1.089.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195A4.052 4.052 0 0014.96 5.61a4.057 4.057 0 00-1.196-2.883zm-.722 5.098L8.58 13.048c-.307.36-.921.36-1.228 0L2.864 7.797a3.072 3.072 0 01-.905-2.187c0-.826.321-1.603.905-2.187a3.091 3.091 0 012.191-.913 3.05 3.05 0 011.957.709c.041.036.408.351.954.351.531 0 .906-.31.94-.34a3.075 3.075 0 014.161.192 3.1 3.1 0 01-.025 4.403z"></path>
+            {/* <path fill="none" d="M0 0h16v16H0z"></path> */}
+            {/* <path
             d="M13.797 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253c-.77.77-1.194 1.794-1.194 2.883s.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195a4.052 4.052 0 001.195-2.883 4.057 4.057 0 00-1.196-2.883z"
           ></path> */}
-        </svg>
-      </Col>
-      <Col className="pip-toggle d-none d-md-flex">
-        <svg width="16" height="16">
-          {/* <g fill="currentColor" fillRule="evenodd"> */}
-          <path d="M1 3v9h14V3H1zm0-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"></path>
-          <path d="M10 8h4v3h-4z"></path>
-          {/* </g> */}
-        </svg>
-      </Col>
+          </svg>
+        </div>
+        <div className="pip-toggle d-none d-md-block">
+          <svg width="16" height="16">
+            {/* <g fill="currentColor" fillRule="evenodd"> */}
+            <path d="M1 3v9h14V3H1zm0-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"></path>
+            <path d="M10 8h4v3h-4z"></path>
+            {/* </g> */}
+          </svg>
+        </div>
+      </div>
     </Row>
   );
 };
--- src/Components/player/Player.jsx
+++ src/Components/player/Player.jsx
@@ -10,11 +10,13 @@ const Player = () => {
   return (
     <Container className="Player" fluid>
       <Row className="px-3 h-100 align-items-center">
-        <Col>{currentSong.album?.id && <NowPlaying />}</Col>
-        <Col>
+        <Col xs={6} sm={4} lg={3}>
+          {currentSong.album?.id && <NowPlaying />}
+        </Col>
+        <Col xs={6} sm={5} lg={6}>
           <PlayerControls />
         </Col>
-        <Col className="d-none d-sm-block">
+        <Col sm={3} lg={3} className="d-none d-sm-block">
           <Volume />
         </Col>
       </Row>
